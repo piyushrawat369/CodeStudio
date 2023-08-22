df49b9dc826610840cd68813095d2d8e103e0115
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2023-08-22 15:29:18 +0530

    Create code.cpp

---
+++ Reverse A Doubly Linked List/code.cpp
@@ -0,0 +1,46 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next,*prev;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+        prev= NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+        this->prev= NULL;
+    }
+    Node(int data, Node* next, Node *prev)
+    {
+        this->data = data;
+        this->next = next;
+        this->prev= prev;
+    }
+};
+
+*/
+
+Node* reverseDLL(Node* head)
+{   
+    Node *curr = head;
+    Node *temp = nullptr;
+    Node *store;
+
+    while(curr){
+        store = curr->next;
+        curr->prev = store;
+        curr->next = temp;
+        temp = curr;
+        curr = store;
+    }
+    return temp;
+}
+
